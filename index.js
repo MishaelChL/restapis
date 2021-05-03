dcec25bda729d697b3bdec28d9827493fca1d7b4
MishaelChL <jose.mishael.chile@gmail.com>
2021-05-03 19:31:45 -0400

    creando el routing

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const routes = require("./routes")
 
 //crear el servidor
 const app = express();
 
-
+//rutas de la app
+app.use("/", routes());
 
 //puerto
 app.listen(3000);
