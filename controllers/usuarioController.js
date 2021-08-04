a54a678edd1973fee83699ae74b1138a61020642
MishaelChL <jose.mishael.chile@gmail.com>
2021-08-03 20:54:22 -0400

    autenticar y generar jwt

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -15,6 +15,34 @@ exports.registrarUsuario = async (req, res) => {
     }
 }
 
-exports.autenticarUsuario = () => {
-    
+exports.autenticarUsuario = async(req, res, next) => {
+    //buscar el usuarioi
+    const usuario = await Usuarios.findOne({ email: req.body.email });
+    if(!usuario){
+        //si el usuario no existe
+        await res.status(401).json({mensaje: 'Ese usuario no existe'});
+        next();
+    }else{
+        //si el usuario existe, verificar si el password es correcto o incorrecto
+        if(!bcrypt.compareSync(req.body.password, usuario.password)){
+            //si el password es incorrecto
+            await res.status(401).json({mensaje: 'Password incorrecto'})
+            next();
+        }else{
+            //password correcto, firmar el token
+            const token = jwt.sign({
+                email: usuario.email,
+                nombre: usuario.nombre,
+                id: usuario._id
+            },
+             'LLAVESECRETA',
+            {
+                expiresIn: '1h'
+            });
+
+            //retornar el token
+            res.json({ token });
+        }
+
+    }
 }
